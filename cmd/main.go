8c6c107f553a28b9ffc0163084256afc6730dbc7
yusukemorita <yusuke.ppq@gmail.com>
2024-07-21 23:44:09 +0200

    implement interactive menu

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"log"
 
-
 	"example.com/git-switch-interactive/internal/git"
+	"github.com/pkg/term"
 )
 
+var (
+	up        []byte = []byte{27, 91, 65}
+	down      []byte = []byte{27, 91, 66}
+	enter     []byte = []byte{13, 0, 0}
+	escape    []byte = []byte{27, 0, 0}
+	control_c []byte = []byte{3, 0, 0}
+)
 
 func main() {
 	branches, err := git.ListBranches()
@@ -15,12 +22,78 @@ func main() {
 		log.Fatalf(err.Error())
 	}
 
-	for _, branch := range branches {
-		if branch.IsCurrent {
-			fmt.Printf("- # %s\n", branch.Name)
-			continue
+	currentIndex := 0
+	drawBranches(branches, currentIndex, false)
+
+	for {
+		keycode, err := readInput()
+		if err != nil {
+			log.Fatal(err.Error())
 		}
 
-		fmt.Printf("- %s\n", branch.Name)
+		if keycodeMatches(keycode, escape) || keycodeMatches(keycode, control_c) {
+			break
+		}
+
+		if keycodeMatches(keycode, up) {
+			currentIndex = currentIndex - 1
+			if currentIndex < 0 {
+				currentIndex += len(branches)
+			}
+			drawBranches(branches, currentIndex, true)
+		}
+
+		if keycodeMatches(keycode, down) {
+			currentIndex = (currentIndex + 1) % len(branches)
+			drawBranches(branches, currentIndex, true)
+		}
+
+		if keycodeMatches(keycode, enter) {
+			log.Printf("switching to branch: %s", branches[currentIndex].Name)
+			break
+		}
 	}
+}
+
+func drawBranches(branches []git.Branch, currentIndex int, redraw bool) {
+	if redraw {
+		// Move the cursor up n lines where n is the number of options, setting the new
+		// location to start printing from, effectively redrawing the option list
+		//
+		// This is done by sending a VT100 escape code to the terminal
+		// @see http://www.climagic.org/mirrors/VT100_Escape_Codes.html
+		fmt.Printf("\033[%dA", len(branches))
+	}
+
+	for index, branch := range branches {
+		if index == currentIndex {
+			fmt.Printf("> %s\n", branch.Name)
+		} else {
+			fmt.Printf("  %s\n", branch.Name)
+		}
+	}
+}
+
+func readInput() ([]byte, error) {
+	terminal, err := term.Open("/dev/tty")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	err = terminal.SetRaw()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	readBytes := make([]byte, 3)
+	_, err = terminal.Read(readBytes)
+
+	terminal.Restore()
+	terminal.Close()
+
+	return readBytes, nil
+}
+
+func keycodeMatches(a, b []byte) bool {
+	return a[0] == b[0] && a[1] == b[1] && a[2] == b[2]
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module example.com/git-switch-interactive
 
 go 1.22.0
+
+require github.com/pkg/term v1.1.0
+
+require golang.org/x/sys v0.22.0 // indirect
